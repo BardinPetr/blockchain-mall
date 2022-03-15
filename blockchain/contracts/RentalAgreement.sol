548f012271c4f66d736ede8ebc85041318aa02f9
BardinPetr <bardin.petr@gmail.com>
2022-03-15 11:12:55 +0300

    Verifications for rent()

--- blockchain/contracts/RentalAgreement.sol
+++ blockchain/contracts/RentalAgreement.sol
@@ -46,8 +46,20 @@ contract RentalAgreement is EIP712 {
 
     function rent(uint deadline, address tenant, uint rentalRate, uint billingPeriodDuration, uint billingsCount, Sign memory landlordSign) public payable {
         if(_inRent) revert("The contract is being in not allowed state");
-        _rentalPermit = RentalPermit(deadline, tenant, rentalRate, billingPeriodDuration, billingsCount);
+
+        RentalPermit memory tmpRP = RentalPermit(deadline, tenant, rentalRate, billingPeriodDuration, billingsCount);
+
+        if(!verify(_landlord, tmpRP, landlordSign)) revert("Invalid landlord sign");
+        if(deadline < block.timestamp) revert("The operation is outdated");
+        if(msg.sender != tenant) revert("The caller account and the account specified as a tenant do not match");
+        if(tenant == _landlord) revert("The landlord cannot become a tenant");
+        if(rentalRate <= 0) revert("Rent amount should be strictly greater than zero");
+        if(billingPeriodDuration <= 0) revert("Rent period should be strictly greater than zero");
+        if(billingsCount <= 0) revert("Rent period repeats should be strictly greater than zero");
+        if(msg.value < rentalRate) revert("Incorrect deposit");
+
         _inRent = true;
         _rentStartTime = block.timestamp;
+        _rentalPermit = tmpRP;
     }
 }
