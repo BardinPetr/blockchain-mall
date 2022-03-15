5e639822d5a468bc1a1e1f44c5a024953746db7f
Bardin Petr <bardin.petr@gmail.com>
2022-03-15 10:58:21 +0300

    Basic rent + started implementing EIP712

--- blockchain/contracts/RentalAgreement.sol
+++ blockchain/contracts/RentalAgreement.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-struct Sign {
-    uint8 v;
-    bytes32 r;
-    bytes32 s;
-}
+import "./EIP712.sol";
 
-contract RentalAgreement {
+contract RentalAgreement is EIP712 {
+
     uint private _roomInternalId;
     address private _landlord;
+    RentalPermit private _rentalPermit;
+    uint private _rentStartTime;
+    bool private _inRent = false;
 
     constructor(uint roomInternalId) {
         _landlord = msg.sender;
@@ -24,21 +24,30 @@ contract RentalAgreement {
         return _landlord;
     }
 
-    function getTenant() public view returns (address) {}
+    function getTenant() public view returns (address) {
+        return _rentalPermit.tenant;
+    }
 
-    function getBillingPeriodDuration() public view returns (uint) {}
+    function getRentalRate() public view returns (uint) {
+        return _rentalPermit.rentalRate;
+    }
 
-    function getRentStartTime() public view returns (uint) {}
+    function getBillingPeriodDuration() public view returns (uint) {
+        return _rentalPermit.billingPeriodDuration;
+    }
 
-    function getRentEndTime() public view returns (uint) {}
+    function getRentStartTime() public view returns (uint) {
+        return _rentStartTime;
+    }
 
-    function getRentalRate() public view returns (uint) {}
+    function getRentEndTime() public view returns (uint) {
+        return _rentStartTime + _rentalPermit.billingsCount * _rentalPermit.billingPeriodDuration;
+    }
 
-    /*
-    RentalPermit(uint256 deadline, address tenant, uint256 rentalRate, uint256 billingPeriodDuration, uint256 billingsCount)
-    EIP712Domain(string name, string version, address verifyingContract) // name = "Rental Agreement", version = "1.0"
-    */
-
-    // function rent(uint deadline, address tenant, uint rentalRate, uint billingPeriodDuration, uint billingsCount, Sign landlordSign) public payable {}
-
+    function rent(uint deadline, address tenant, uint rentalRate, uint billingPeriodDuration, uint billingsCount, Sign memory landlordSign) public payable {
+        if(_inRent) revert("The contract is being in not allowed state");
+        _rentalPermit = RentalPermit(deadline, tenant, rentalRate, billingPeriodDuration, billingsCount);
+        _inRent = true;
+        _rentStartTime = block.timestamp;
+    }
 }
