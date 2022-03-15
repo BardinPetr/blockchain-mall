ef13e9b4dd2787d9ad3ad61df1a7a4e385c040cd
Bardin Petr <bardin.petr@gmail.com>
2022-03-15 14:39:44 +0300

    docker-related fixes

--- blockchain/contracts/RentalAgreement.sol
+++ blockchain/contracts/RentalAgreement.sol
@@ -45,13 +45,11 @@ contract RentalAgreement is EIP712 {
     }
 
     function rent(uint deadline, address tenant, uint rentalRate, uint billingPeriodDuration, uint billingsCount, Sign memory landlordSign) public payable {
-    // function rent(uint deadline, address tenant, uint rentalRate, uint billingPeriodDuration, uint billingsCount, uint8 v, bytes32 r, bytes32 s) public payable {
         if(_inRent) revert("The contract is being in not allowed state");
 
         RentalPermit memory tmpRP = RentalPermit(deadline, tenant, rentalRate, billingPeriodDuration, billingsCount);
 
         if(!verify(_landlord, tmpRP, landlordSign)) revert("Invalid landlord sign");
-        // if(!verify(_landlord, tmpRP, Sign(v,r,s))) revert("Invalid landlord sign");
         if(deadline < block.timestamp) revert("The operation is outdated");
         if(msg.sender != tenant) revert("The caller account and the account specified as a tenant do not match");
         if(tenant == _landlord) revert("The landlord cannot become a tenant");
