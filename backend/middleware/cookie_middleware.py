785a0a02275c53b660780ed5d7b27e674ad42a35
Artemiy Prikupets <artemprikupets@gmail.com>
2022-03-16 16:55:45 +0300

    fix: omg again cookie fix

--- backend/middleware/cookie_middleware.py
+++ backend/middleware/cookie_middleware.py
@@ -4,6 +4,7 @@ from starlette.middleware.base import BaseHTTPMiddleware
 class CookieMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         response = await call_next(request)
+        response.headers['Set-Cookie'] = 'access_token_cookie=bipki'
         response.headers['Cookie'] = 'access_token_cookie=bipki'
         print("IN CookieMiddleware - response headers:", response.headers)
         return response
