7b73e28d9c14822715ce96c0822409045b92e8d9
BardinPetr <bardin.petr@gmail.com>
2022-03-16 13:34:32 +0300

    Fix

--- blockchain/contracts/RentalAgreement.sol
+++ blockchain/contracts/RentalAgreement.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.11;
 
 import "./EIP712.sol";
 
-struct Cashier {
-    bool valid;
-    uint256 nonce;
-}
+    struct Cashier {
+        bool valid;
+        uint256 nonce;
+    }
 
 contract RentalAgreement is EIP712 {
     event PurchasePayment(uint amount);
@@ -56,18 +56,18 @@ contract RentalAgreement is EIP712 {
     }
 
     function rent(uint deadline, address tenant, uint rentalRate, uint billingPeriodDuration, uint billingsCount, Sign memory landlordSign) public payable {
-        if(_inRent) revert("The contract is being in not allowed state");
+        if (_inRent) revert("The contract is being in not allowed state");
 
         RentalPermit memory tmpRP = RentalPermit(deadline, tenant, rentalRate, billingPeriodDuration, billingsCount);
 
-        if(!verifyRentalPermit(_landlord, tmpRP, landlordSign)) revert("Invalid landlord sign");
-        if(deadline < block.timestamp) revert("The operation is outdated");
-        if(msg.sender != tenant) revert("The caller account and the account specified as a tenant do not match");
-        if(tenant == _landlord) revert("The landlord cannot become a tenant");
-        if(rentalRate <= 0) revert("Rent amount should be strictly greater than zero");
-        if(billingPeriodDuration <= 0) revert("Rent period should be strictly greater than zero");
-        if(billingsCount <= 0) revert("Rent period repeats should be strictly greater than zero");
-        if(msg.value < rentalRate) revert("Incorrect deposit");
+        if (!verifyRentalPermit(_landlord, tmpRP, landlordSign)) revert("Invalid landlord sign");
+        if (deadline < block.timestamp) revert("The operation is outdated");
+        if (msg.sender != tenant) revert("The caller account and the account specified as a tenant do not match");
+        if (tenant == _landlord) revert("The landlord cannot become a tenant");
+        if (rentalRate <= 0) revert("Rent amount should be strictly greater than zero");
+        if (billingPeriodDuration <= 0) revert("Rent period should be strictly greater than zero");
+        if (billingsCount <= 0) revert("Rent period repeats should be strictly greater than zero");
+        if (msg.value < rentalRate) revert("Incorrect deposit");
 
         _inRent = true;
         _rentStartTime = block.timestamp;
@@ -75,11 +75,11 @@ contract RentalAgreement is EIP712 {
     }
 
     function removeCashier(address cashierAddr) public {
-        if(msg.sender != _rentalPermit.tenant) revert("You are not a tenant");
-        if(_cashierNonces[cashierAddr] == 0) revert("Unknown cashier");
+        if (msg.sender != _rentalPermit.tenant) revert("You are not a tenant");
+        if (_cashierNonces[cashierAddr] == 0) revert("Unknown cashier");
 
         uint i = 0;
-        for(; i < _cashierAddresses.length; i++) if(_cashierAddresses[i] == cashierAddr) break;
+        for (; i < _cashierAddresses.length; i++) if (_cashierAddresses[i] == cashierAddr) break;
         _cashierAddresses[i] = _cashierAddresses[_cashierAddresses.length - 1];
         _cashierAddresses.pop();
 
@@ -87,9 +87,9 @@ contract RentalAgreement is EIP712 {
     }
 
     function addCashier(address addr) public {
-        if(msg.sender != _rentalPermit.tenant) revert("You are not a tenant");
-        if(addr == _landlord) revert("The landlord cannot become a cashier");
-        if(addr == 0x0000000000000000000000000000000000000000) revert("Zero address cannot become a cashier");
+        if (msg.sender != _rentalPermit.tenant) revert("You are not a tenant");
+        if (addr == _landlord) revert("The landlord cannot become a cashier");
+        if (addr == 0x0000000000000000000000000000000000000000) revert("Zero address cannot become a cashier");
 
         _cashierAddresses.push(addr);
         _cashierNonces[addr] = _curCashierNonce++;
@@ -107,12 +107,12 @@ contract RentalAgreement is EIP712 {
         Ticket memory t = Ticket(deadline, nonce, value);
         address cashier = getTicketIssuer(t, cashierSign);
 
-         if(_cashierNonces[cashier] == 0) revert("Unknown cashier");
-         if(deadline < block.timestamp) revert("The operation is outdated");
-         if(nonce != _cashierNonces[cashier]) revert("Invalid nonce");
-         if(msg.value != value) revert("Invalid value");
-//         if((deadline > getRentEndTime()) || (block.timestamp > _rentalPermit.deadline))
-//             revert("The contract is being in not allowed state");
+        if (_cashierNonces[cashier] == 0) revert("Unknown cashier");
+        if (deadline < block.timestamp) revert("The operation is outdated");
+        if (nonce != _cashierNonces[cashier]) revert("Invalid nonce");
+        if (msg.value != value) revert("Invalid value");
+        if (block.timestamp > getRentEndTime()) revert("The contract is being in not allowed state");
+        // if(block.timestamp > _rentalPermit.deadline) revert("The contract is being in not allowed state");
 
         _cashierNonces[cashier]++;
         emit PurchasePayment(value);
