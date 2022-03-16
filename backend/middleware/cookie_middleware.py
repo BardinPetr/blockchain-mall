219b9f81e8c5dbe9127db784c0bc653253e03c5e
Artemiy Prikupets <artemprikupets@gmail.com>
2022-03-16 16:47:31 +0300

    feature: added some middleware logging

--- backend/middleware/cookie_middleware.py
+++ backend/middleware/cookie_middleware.py
@@ -5,4 +5,5 @@ class CookieMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         response = await call_next(request)
         response.headers['Cookie'] = 'access_token_cookie=bipki'
+        print("IN CookieMiddleware - response headers:", response.headers)
         return response
