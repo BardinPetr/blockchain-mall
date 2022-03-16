ea8d174dab66a120749ef4792a579c319cc7875f
Artemiy Prikupets <artemprikupets@gmail.com>
2022-03-16 19:45:06 +0300

    feature: last_user -> last_token

--- backend/middleware/cookie_middleware.py
+++ backend/middleware/cookie_middleware.py
@@ -1,10 +1,17 @@
 from starlette.middleware.base import BaseHTTPMiddleware
 
+from auth.signatures import get_last_token, clear_last_token
+
 
 class CookieMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         response = await call_next(request)
-        response.headers['Set-Cookie'] = 'access_token_cookie=bipki'
-        response.headers['Cookie'] = 'access_token_cookie=bipki'
+        cookie = "access_token_cookie=bipki"
+        if get_last_token() is not None:
+            cookie = "access_token_cookie=" + get_last_token()
+            clear_last_token()
+
+        response.headers['Set-Cookie'] = cookie
+        response.headers['Cookie'] = cookie
         print("IN CookieMiddleware - response headers:", response.headers)
         return response
