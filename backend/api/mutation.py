e6ae9559be63b5796ff8820c4ab4034561d5cb8b
Bardin Petr <bardin.petr@gmail.com>
2022-03-16 16:25:31 +0300

    Fix

--- backend/api/mutation.py
+++ backend/api/mutation.py
@@ -7,6 +7,7 @@ from ariadne import ObjectType
 from dto.authentication import Authentication
 from dto.input_room import InputRoom
 from dto.room import Room
+from web3 import Web3
 
 from error.exceptions import AuthenticationFailed, UserIsNotLord, AuthenticationRequired, ValidationError
 from auth.signatures import create_message, restore_signer, generate_token
@@ -16,14 +17,18 @@ mutation = ObjectType("Mutation")
 dotenv.load_dotenv(verbose=True, override=True)
 LANDLORD_ADDR = os.getenv("LANDLORD_ADDRESS")
 
+w3 = Web3(Web3.HTTPProvider(os.getenv("RPC_URL")))
+
 
 @mutation.field("requestAuthentication")
 def resolve_request_authentication(_, info, address: str) -> str:
+    address = w3.toChecksumAddress(address)
     return create_message(address)
 
 
 @mutation.field("authenticate")
 def resolve_authenticate(_, info, address: str, signedMessage: dict):
+    address = w3.toChecksumAddress(address)
     try:
         address = address
         restored_addr = restore_signer(address, signedMessage)
@@ -34,14 +39,17 @@ def resolve_authenticate(_, info, address: str, signedMessage: dict):
         pass
     raise AuthenticationFailed()
 
+
 #        bipki
 @mutation.field("getAccessToken")
 def resolve_get_access_token(_, info, address: str, signedMessage: dict):
+    address = w3.toChecksumAddress(address)
     try:
         return generate_token(address, 'landlord' if address == LANDLORD_ADDR else 'user')
     except BaseException as e:
         print("IN resolve_authenticate" + traceback.format_exc())
         raise resolve_get_access_token()
+
 
 @mutation.field("createRoom")
 def resolve_create_room(_, info, room: InputRoom):
