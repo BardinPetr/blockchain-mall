43503b80db2ae4468356d1a8f417f2c45bcff9d9
Artemiy Prikupets <artemprikupets@gmail.com>
2022-03-16 12:15:15 +0300

    feature: added AuthenticationRequired & UserIsNotLord exceptions

--- backend/error/exceptions.py
+++ backend/error/exceptions.py
@@ -1,3 +1,13 @@
 class AuthenticationFailed(Exception):
     def __init__(self):
         super().__init__("Authentication failed")
+
+
+class AuthenticationRequired(Exception):
+    def __init__(self):
+        super().__init__("Authentication required")
+
+
+class UserIsNotLord(Exception):
+    def __init__(self):
+        super().__init__("This method is available only for the landlord")
