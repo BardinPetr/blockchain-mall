17c5e7f231914a6f9f54f6ab58ba40264632b482
Layerex <layerex@dismail.de>
2022-03-16 15:44:01 +0300

    Fixes [skip ci]

--- frontend/src/views/Rooms.js
+++ frontend/src/views/Rooms.js
@@ -1,37 +1,36 @@
-import { useQuery } from "@apollo/client";
+import { useQuery, useLazyQuery } from "@apollo/client";
 import { useState } from "react";
 
 import Field from "./Field";
 
-import { AUTHENTICATION, GET_ALL_ROOMS, STATUSES } from "../gql/queries";
+import { AUTHENTICATION, GET_ALL_ROOMS, GET_TENANT_ROOMS, STATUSES } from "../gql/queries";
 
 function Rooms() {
   const [address, setAddress] = useState(undefined);
   const [isLandlord, setIsLandlord] = useState(undefined);
   const [rooms, setRooms] = useState([]);
+  const [roomQuery] = useLazyQuery(isLandlord ? GET_ALL_ROOMS : GET_TENANT_ROOMS, {
+    fetchPolicy: "no-cache",
+    variables: {
+      address: isLandlord ? undefined : address,
+    },
+    onCompleted: ({ data, }) => {
+      setRooms(data);
+      console.log(data);
+    },
+  });
   const { error: isAuthError } = useQuery(AUTHENTICATION, {
     fetchPolicy: "no-cache",
     onCompleted: ({ data }) => {
       setAddress(data.address);
       setIsLandlord(data.landlord);
+      roomQuery();
     },
   });
-  useEffect(() => {
-    useQuery(isLandlord ? GET_ALL_ROOMS : GET_TENANT_ROOMS, {
-      fetchPolicy: "no-cache",
-      variables: {
-        address: isLandlord ? undefined : address,
-      },
-      onCompleted: ({ data }) => {
-        setRooms(data);
-        console.log(data);
-      },
-    });
-  }, [isLandlord]);
   return (
     <>
       {isAuthError && <p>Auth error!</p>}
-      {isError && <p>Error!</p>}
+      {/* {isError && <p>Error!</p>} */}
       {isLandlord && (
         <a className="rooms__create" href="/rooms/create/">
           rooms__create
@@ -40,7 +39,7 @@ function Rooms() {
       {rooms.map((data) => {
         <div id={`room-${data.id}`} className="room-card">
           <Field k="room-card__name" v={data.publicName} />
-          <Field k="room-card__status" v={data.status} />
+          <Field k="room-card__status" v={STATUSES[data.status]} />
           <a className="room-card__details" href={`/room/${data.id}`}>
             room-card__details
           </a>
