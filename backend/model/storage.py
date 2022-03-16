100aa85ce7050e96e18fdf6bdd1dece83bf1a5cc
Artemiy Prikupets <artemprikupets@gmail.com>
2022-03-16 20:52:16 +0300

    feature: made storage.rooms

---
+++ backend/model/storage.py
@@ -0,0 +1,33 @@
+rooms = []
+
+
+def add_room(room):
+    global rooms
+    room['id'] = len(rooms) + 1
+    rooms.append(room)
+    print("IN add_room - room: ", room)
+    return room
+
+
+def get_rooms():
+    global rooms
+    print("IN get_rooms - rooms: ", rooms)
+    return rooms
+
+
+def remove_room(id):
+    global rooms
+    for room in rooms:
+        if room['id'] == id:
+            print("IN remove_room - room: ", room)
+            del room
+    print("IN remove_room - not found room with id: ", id)
+
+
+def get_room_by_id(id):
+    global rooms
+    for room in rooms:
+        if room['id'] == id:
+            print("IN get_room_by_id - room: ", room)
+            return room
+    print("IN get_room_by_id - not found room with id: ", id)
