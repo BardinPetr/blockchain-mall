844a8154308875827ad4ff43eaf8fc56e016e270
Artemiy Prikupets <artemprikupets@gmail.com>
2022-03-16 12:14:30 +0300

    feature: added validation for Authorization header; added some logs in resolve_authenticate

--- backend/api/mutation.py
+++ backend/api/mutation.py
@@ -1,11 +1,14 @@
 import os
 import time
+import traceback
 
 import dotenv
 from ariadne import ObjectType
 from dto.authentication import Authentication
+from dto.input_room import InputRoom
+from dto.room import Room
 
-from error.exceptions import AuthenticationFailed
+from error.exceptions import AuthenticationFailed, UserIsNotLord, AuthenticationRequired
 from auth.signatures import create_message, restore_signer
 
 mutation = ObjectType("Mutation")
@@ -27,6 +30,47 @@ def resolve_authenticate(_, info, address: str, signedMessage: dict):
         restored_addr = restore_signer(address, signedMessage).lower()
         if restored_addr == address:
             return Authentication(address, address == LANDLORD_ADDR.lower())
-    except:
+    except BaseException as e:
+        print("IN resolve_authenticate" + traceback.format_exc())
         pass
     raise AuthenticationFailed()
+
+
+@mutation.field("createRoom")
+def resolve_create_room(_, info, room: InputRoom):
+    headers = info.context['request'].headers
+    access_token = headers.get("Authorization")
+    if access_token is None:
+        raise AuthenticationRequired()
+
+
+@mutation.field("setRoomContractAddress")
+def resolve_set_room_contract_address(_, info, id: int, address: str):
+    headers = info.context['request'].headers
+    access_token = headers.get("Authorization")
+    if access_token is None:
+        raise AuthenticationRequired()
+
+
+@mutation.field("editRoom")
+def resolve_edit_room(_, info, id: int, room: InputRoom):
+    headers = info.context['request'].headers
+    access_token = headers.get("Authorization")
+    if access_token is None:
+        raise AuthenticationRequired()
+
+
+@mutation.field("removeRoom")
+def resolve_remove_room(_, info, id: int):
+    headers = info.context['request'].headers
+    access_token = headers.get("Authorization")
+    if access_token is None:
+        raise AuthenticationRequired()
+
+
+@mutation.field("setRoomPublicName")
+def resolve_remove_room(_, info, id: int, publicName: str):
+    headers = info.context['request'].headers
+    access_token = headers.get("Authorization")
+    if access_token is None:
+        raise AuthenticationRequired()
