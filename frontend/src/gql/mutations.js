8a4ced44b8d5a46a7271ab221f22f76cabb04769
Layerex <layerex@dismail.de>
2022-03-17 14:00:06 +0300

    Fix auth!!!!! and createRoom!!!

--- frontend/src/gql/mutations.js
+++ frontend/src/gql/mutations.js
@@ -7,32 +7,26 @@ export const REQUEST_AUTHENTICATION = gql`
 `;
 
 export const AUTHENTICATE = gql`
-    mutation Authenticate($address: String!, $v: String!, $r: String!, $s: String!) {
-        authentication: authenticate(
-            address: $address
-            signedMessage: {
-                v: $v,
-                r: $r,
-                s: $s
-            }
-        ) {
-            address
-            isLandlord
-        }
+  mutation Authenticate(
+    $address: String!
+    $v: String!
+    $r: String!
+    $s: String!
+  ) {
+    authentication: authenticate(
+      address: $address
+      signedMessage: { v: $v, r: $r, s: $s }
+    ) {
+      address
+      isLandlord
     }
+  }
 `;
 
 export const GET_ACCESS_TOKEN = gql`
-    mutation GetAccessToken($address: String!, $v: String!, $r: String!, $s: String!) {
-        token: getAccessToken(
-            address: $address
-            signedMessage: {
-                v: $v,
-                r: $r,
-                s: $s
-            }
-        )
-    }
+  mutation GetAccessToken($address: String!) {
+    token: getAccessToken(address: $address)
+  }
 `;
 
 export const CREATE_ROOM = gql`
